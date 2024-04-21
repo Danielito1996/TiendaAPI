70705eee086ad2b87305eabe040435d697b4f45f
Esteban <daniel.imbert96@hotmail.com>
2024-04-21 16:50:45 -0400

    First Commit

---
+++ TiendaAPI/Servicios/Aplicacion/Logs/IServiciosLogs.cs
@@ -0,0 +1,7 @@
+﻿namespace TiendaAPI.Servicios.Aplicacion.Logs
+{
+    public interface IServiciosLogs
+    {
+        Task Log(string message);
+    }
+}
---
+++ TiendaAPI/Servicios/Aplicacion/Logs/ServiciosLogs.cs
@@ -0,0 +1,24 @@
+﻿namespace TiendaAPI.Servicios.Aplicacion.Logs
+{
+    public class ServiciosLogs:IServiciosLogs
+    {
+        private string _logFilePath;
+        public ServiciosLogs()
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string logFolder = Path.Combine(docPath, "GestorFinanciero", "Log");
+            if (!Directory.Exists(logFolder))
+            {
+                Directory.CreateDirectory(logFolder);
+            }
+            _logFilePath = Path.Combine(logFolder, "app.log");
+        }
+        public async Task Log(string message)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(_logFilePath, true))
+            {
+                streamWriter.WriteLine($"{DateTime.Now}: {message}");
+            }
+        }
+    }
+}
